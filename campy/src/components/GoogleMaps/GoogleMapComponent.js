4a4160b7510d603c88ad6bc41bdf252d2529a57b
anusca <anna.arsentieva@gmail.com>
2020-08-07 10:13:58 -0700

    add style for google maps and use users current locations via HTML5 geolocation

--- campy/src/components/GoogleMaps/GoogleMapComponent.js
+++ campy/src/components/GoogleMaps/GoogleMapComponent.js
@@ -1,27 +1,71 @@
 import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
+import mapStyle from "./mapStyle";
 
 const containerStyle = {
   width: "100vw",
   height: "40vh",
 };
 
-const center = {
-  lat: 30.2672,
-  lng: -97.7431,
+const options = {
+  styles: mapStyle,
+  disableDefaultUI: true,
 };
-const key = 'AIzaSyADACPgre6kS5Osy4cDzz15p5jF5sAwbqQ';
+
+const libraries = ["places"]
+
+let lat=0;
+let lng=0;
+
+function showCoords(position) {
+  lat = position.coords.latitude;
+  lng = position.coords.longitude;
+
+}
+function geo_error(error) {
+  switch(error.code) {
+      case error.TIMEOUT:
+          alert('Geolocation Timeout');
+          break;
+      case error.POSITION_UNAVAILABLE:
+          alert('Geolocation Position unavailable');
+          break;
+      case error.PERMISSION_DENIED:
+          alert('Geolocation Permission denied');
+          break;
+      default:
+          alert('Geolocation returned an unknown error code: ' + error.code);
+  }
+}
+
+function getCurrentPosition() {
+    navigator.geolocation.getCurrentPosition(showCoords, geo_error)
+    console.log("got location")
+}
+
+getCurrentPosition()
 
 export const GoogleMapComponent = () => {
+  const {isLoaded, loadError} = useLoadScript ({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  if (loadError) {
+    return "Error loading maps";
+  }
+  if ( !isLoaded) {
+    return "Loading Maps";
+  }
+
   return (
     <div>
-      <LoadScript googleMapsApiKey={key}>
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+
+        <GoogleMap mapContainerStyle={containerStyle} center={{lat, lng}} zoom={10} options= {options}>
           {/* Child components, such as markers, info windows, etc. */}
           <></>
         </GoogleMap>
-      </LoadScript>
+
     </div>
   );
 }
-
---
+++ campy/src/components/GoogleMaps/places.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        "id": 1,
+        "campgroundName": "Silver Falls Group Site",
+        "lat": 47.959,
+        "lng": -120.539
+
+    },
+    {
+        "id": 2,
+        "campgroundName": "Okanogan-Wenatchee National Forest",
+        "lat": 48.0937,
+        "lng": -120.313
+
+    },
+    {
+        "id":3,
+        "campgroundName": "Pine Flats Group Campground",
+        "lat": 47.7586,
+        "lng": -120.425
+
+    },
+    {   "id":4,
+        "campgroundName": "Cottonwood Cabin",
+        "lat": 48.02095,
+        "lng": -120.641222
+
+    },
+    {
+        "id": 5,
+        "campgroundName": "Grouse Creek Group Site",
+        "lat": 47.896667,
+        "lng": -120.698889
+
+    },
+    {
+        "id": 6,
+        "campgroundName": "Grouse Creek Group Site",
+        "lat": 47.79960,
+        "lng": -120.716
+
+    },
+
+]
