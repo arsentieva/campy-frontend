dd290fbc5a83f7cc363d332203394e428c5f90b9
anusca <anna.arsentieva@gmail.com>
2020-08-07 16:09:08 -0700

    position the google maps on the right side of the page

--- campy/src/components/GoogleMaps/GoogleMapComponent.js
+++ campy/src/components/GoogleMaps/GoogleMapComponent.js
@@ -1,5 +1,6 @@
 import React, {useState, useRef, useCallback} from "react";
 import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
+import { Grid, Typography, IconButton } from "@material-ui/core";
 import mapStyle from "./mapStyle";
 import places from "./places";
 
@@ -74,34 +75,46 @@ export const GoogleMapComponent = () => {
   let imageUrl= "http://maps.google.com/mapfiles/kml/shapes/campground.png";
   return (
     <div>
-     <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={{lat, lng}}
-      zoom={8}
-      options= {options}
-      onLoad={onMapLoad}
-     >
-          {
-          places.map((place)=>(
-            <Marker key = {place.id}
-            position = { {lat: place.lat, lng: place.lng}}
-            icon = {{
-              url: imageUrl,
-              scaledSize: new window.google.maps.Size(30, 30),
-              origin: new window.google.maps.Point(0, 0),
-              anchor: new window.google.maps.Point(15, 15),
-            }}
-            onClick = {()=> { setSelected(place)}}
-            />
-          ))}
-          {selected ? (
-          <InfoWindow position= {{ lat:selected.lat, lng:selected.lng}} onCloseClick={()=> setSelected(null)}>
-            <div>
-              <h2> {selected.campgroundName} </h2>
-            </div>
-          </InfoWindow>) : null}
-          <></>
-        </GoogleMap>
+        <Grid container style={{ minHeight: "100vh" }}>
+        <Grid item xs={4} />
+        <Grid
+        item
+        container
+        xs={8}
+        alignContent="flex-end"
+        justify="center"
+        direction="column"
+      >
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={{lat, lng}}
+              zoom={8}
+              options= {options}
+              onLoad={onMapLoad}
+            >
+                  {
+                  places.map((place)=>(
+                    <Marker key = {place.id}
+                    position = { {lat: place.lat, lng: place.lng}}
+                    icon = {{
+                      url: imageUrl,
+                      scaledSize: new window.google.maps.Size(30, 30),
+                      origin: new window.google.maps.Point(0, 0),
+                      anchor: new window.google.maps.Point(15, 15),
+                    }}
+                    onClick = {()=> { setSelected(place)}}
+                    />
+                  ))}
+                  {selected ? (
+                  <InfoWindow position= {{ lat:selected.lat, lng:selected.lng}} onCloseClick={()=> setSelected(null)}>
+                    <div>
+                      <h2> {selected.campgroundName} </h2>
+                    </div>
+                  </InfoWindow>) : null}
+                  <></>
+                </GoogleMap>
+             </Grid>
+        </Grid>
     </div>
   );
 }
