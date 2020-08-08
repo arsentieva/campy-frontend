f924f03a79362775c65bf6ec689cfb0efec0b816
anusca <anna.arsentieva@gmail.com>
2020-08-07 22:15:02 -0700

    add card for locations

--- campy/src/components/GoogleMaps/places.js
+++ campy/src/components/GoogleMaps/places.js
@@ -1,6 +1,9 @@
 export default [
     {
         "id": 1,
+        "rating": 4,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Silver Falls Group Site",
         "lat": 47.959,
         "lng": -120.539
@@ -8,155 +11,233 @@ export default [
     },
     {
         "id": 2,
+        "rating": 4.5,
+        "maxDays":3,
+        "state": "Washington",
         "campgroundName": "Okanogan-Wenatchee National Forest",
         "lat": 48.0937,
         "lng": -120.313
     },
     {
         "id":3,
+        "rating": 3.5,
+        "maxDays":4,
+        "state": "Washington",
         "campgroundName": "Pine Flats Group Campground",
         "lat": 47.7586,
         "lng": -120.425
     },
     {   "id":4,
-        "campgroundName": "Cottonwood Cabin",
+    "rating": 5,
+    "maxDays":1,
+    "state": "Washington",
+    "campgroundName": "Cottonwood Cabin",
         "lat": 48.02095,
         "lng": -120.641222
     },
     {
         "id": 5,
+        "rating": 3,
+        "maxDays":3,
+        "state": "Washington",
         "campgroundName": "Grouse Creek Group Site",
         "lat": 47.896667,
         "lng": -120.698889
     },
     {
         "id": 6,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Nason Creek Campground",
         "lat": 47.79960,
         "lng": -120.716
     },
     {
         "id": 7,
+        "rating": 4,
+        "maxDays":3,
+        "state": "Washington",
         "campgroundName": "Tumwater Group Site",
         "lat": 47.678056,
         "lng": -120.730833
     },
     {
         "id": 8,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Tumwater Group Site",
         "lat": 47.586111,
         "lng": -120.807028
     },
     {
         "id": 9,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Icicle Group Campground",
         "lat": 47.586111,
         "lng": -120.807028
     },
     {
         "id": 10,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Bridge Creek Group Site",
         "lat": 47.563889,
         "lng": -120.780833
     },
     {
         "id": 11,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Eightmile Campground",
         "lat": 47.550833,
         "lng": -120.766111
     },
     {
         "id": 12,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Eightmile Group Site",
         "lat": 47.550833,
         "lng": -120.766111
     },
     {
         "id": 13,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Chatter Creek Group Site",
         "lat": 47.608333,
         "lng": -120.885
     },
     {
         "id": 14,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Enchantment Permit Area",
         "lat": 47.4866667,
         "lng": -120.7866667
     },
     {
         "id": 15,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Mitchell Creek Loop ATV/ORV Route",
         "lat": 47.996761,
         "lng": -120.151466
     },
     {
         "id": 16,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Rufus Woods Lake",
         "lat": 47.998795,
         "lng": -119.628172
     },
     {
         "id": 17,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Sulphur Creek Campground",
         "lat": 48.248509,
         "lng": -121.194058
     },
     {
         "id": 18,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Lake Washington Ship Canal",
         "lat": 47.665794,
         "lng": -122.396860
     },
     {
         "id": 19,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Bedal Campground",
         "lat": 48.096843,
         "lng": -121.386943
     },
     {
         "id": 20,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Boardman Group Camp",
         "lat": 48.069965,
         "lng": -121.676385
     },
     {
         "id": 21,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Boulder Creek Campground",
         "lat": 48.715415,
         "lng": -121.694457
     },
     {
         "id": 22,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Cayuse Horse Camp",
         "lat": 47.399582,
         "lng": -121.094828
     },
     {
         "id": 23,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Indian Flat Group Site",
         "lat": 46.979977,
         "lng": -121.148972
     },
     {
         "id": 24,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Kachess",
         "lat": 47.354840,
         "lng": -121.243420
     },
     {
         "id": 25,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Lodgepole Campground",
         "lat": 46.915833,
         "lng": -121.384444
     },
     {
         "id": 26,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Mud Mountain Dam (White River)",
         "lat": 47.140000,
         "lng": -121.931670
     },
     {
         "id": 27,
+        "rating": 4.5,
+        "maxDays":2,
+        "state": "Washington",
         "campgroundName": "Klondike Gold Rush Visitor Center",
         "lat": 47.599354,
         "lng": -122.332113
