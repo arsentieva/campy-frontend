c6a61917e636868ac2df78339c6609353041e142
arsentieva <anna.arsentieva@gmail.com>
2020-09-10 10:35:06 -0700

    add locations and location to the application context

--- src/AppWithContext.js
+++ src/AppWithContext.js
@@ -2,23 +2,21 @@ import React, { useState } from "react";
 import App from "./App";
 import { CampyContext } from "./CampyContext";
 import url from './config';
-import { Report } from "@material-ui/icons";
 
 export const AppWithContext = () => {
   const accessToken = localStorage.getItem("access_token");
   const [authToken, setAuthToken] = useState(accessToken);
-  const [isLoggedIn, setIsLoggedIn] = useState(!accessToken);
   const [currentUser, setCurrentUser] = useState();
+  const [locations, setLocations] = useState();
+  const [location, setLocation] = useState();
   
   const login = (token) => {
     window.localStorage.setItem("campy_token", token);
     setAuthToken(token);
-    setIsLoggedIn(true);
   };
   const logOut = () => {
     window.localStorage.clear();
     setAuthToken(null);
-    setIsLoggedIn(false);
   };
 
   const getUser = async (userID) => {
@@ -39,9 +37,35 @@ export const AppWithContext = () => {
     };
   };
 
+  const loadLocations = async () => {
+    try {
+    const res = await fetch(`${url}/locations`);
+    if(!res.ok){
+      throw res;
+    }
+    const allLocations = await res.json();
+    setLocations(allLocations.locations);
+   } catch (error) {
+     console.log(error);
+   }
+  }
+
+  const loadLocation = async (id) => {
+    try {
+    const res = await fetch(`${url}/location/${id}`);
+    if(!res.ok){
+      throw res;
+    }
+    const selectedActivity = await res.json();
+    setLocation(selectedActivity.location);
+   } catch (error) {
+     console.log(error);
+   }
+  }
+
   
   return (
-    <CampyContext.Provider value={{ authToken, isLoggedIn, login, logOut, currentUser, getUser }}>
+    <CampyContext.Provider value={{ authToken, login, logOut, currentUser, getUser, loadLocations, locations, location, loadLocation }}>
       <App />
     </CampyContext.Provider>
   );
