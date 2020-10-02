19ae3194a73cf6b0b911443ccc48024e35820399
Aaron Bruce <aaronbruce555@gmail.com>
2020-10-02 17:07:13 -0400

    Add Clear Dates for when Dates are entered

--- src/components/New-Calendar.js
+++ src/components/New-Calendar.js
@@ -114,6 +114,7 @@ class NewCalendar extends Component {
             onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
             focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
             onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+            showClearDates={true}
           />
         </Grid>
         <Grid item xs={2} className={classes.stem}>
